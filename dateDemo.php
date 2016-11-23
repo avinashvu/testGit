5b4aac16f3506768bdbab250b4f677aebbe433ae
avinashvu <avinash@amstpl.in>
2016-11-23 15:12:46 +0530

    added date

---
+++ dateDemo.php
@@ -0,0 +1,107 @@
+<html><body>
+		<?php 
+			//echo $date = date("Y-m-d");
+			//echo $NewDate=Date('Y-m-d', strtotime("+7 days"));
+		?>
+		<br>
+		<?php
+			//echo $date = date('d-M-Y');
+		?>
+		<br>
+		<?php
+			//echo $time = date('h:m:s A',time() + 45000);
+		?>
+		<br>
+		<?php
+			//echo $x;
+			//echo $date = date('D', $x);
+		?>
+		<br>
+		<?php
+			//echo $size = str_replace('"',"''",'38"/95CM')."<br><br><br><br><br><br>";
+			//$customerID = '125689';
+			//echo $cust3digit = substr($customerID, -3);
+		?>
+		<br>
+		<?php
+			 //$date = date('Y-m-d');
+			 /*$date = date("Y-m-d", time() + 45000);
+			 echo $time = date("h:i:s A", time() + 20000)."<br>";
+			 echo $oldDate = date('Y-m-d',strtotime($date."-15 days"));*/
+			 /*
+			 	$time  = time();
+				//$fileName = 'sitemap.txt';
+				$file = fopen('sitemap.txt', 'a');
+				$sitemap = $_SERVER['DOCUMENT_ROOT'].'/sitemap.txt';
+				echo filemtime($sitemap);
+				if ($time - filemtime($sitemap) >= 1*24*60*60) { // 1 days
+					
+					$data = 'Executed the code'.PHP_EOL;
+					fwrite($file, $data);
+					echo "Code executes only once per day.";
+
+				}else{
+					$data = 'Not Executed the code'.PHP_EOL;
+					//fwrite($file, $data);
+					echo "Code executes every time script is called.";
+				}*/
+			 
+			// echo $date = date("Y-m-d", time() + 45000)."<br>";
+			// echo $date = date("D", time() + 45000)."<br>";
+			// echo $time = date("h:i:s A", time() + 45000);
+			 
+			 //$date = date("Y-m-d", time() + 45000);
+	
+		// echo date('d M Y',strtotime($date."+15 days"));
+		
+		//echo $positionOfCustomerNumber = strpos(" ","0");
+			//echo $date1 = date('d-M-Y',strtotime($date));
+			//echo $date2 = date('Y-m-d',strtotime($date."-1 day"));
+			
+			//echo $date = date("Y-m-d", time() + 22500);
+			//echo $oldDate = date("Y-m-d", strtotime($date, "-7 days"));
+			//echo $oldDate = date("Y-m-d", strtotime($date." -6 day"));
+			
+			//echo $endDate = strtotime('+ 1 year', $date);
+			/*$name = "avinash";
+			echo $name. "<br>";
+			$NewName = ucwords($name);
+			echo $NewName;*/
+			//$date = date("Y-m-d", time() + 45000);
+			$date = '10/13/2016';
+			//echo $expiryDate = date('Y-m-d',strtotime($date."-12 months"));
+			//echo $oneMonthBeforeexpireDate = date('Y-m-d',strtotime($expiryDate."+1 months"));
+			//echo $pointsexpireDate = date('Y-m-d',strtotime($date."+1 months"));
+			//echo $date1 = date("Y-m-01", time() + 45000);
+			
+			//echo $reference = sha1(md5(time()));
+			//echo $mDate = date("y-M-d", time() + 45000);
+			echo $mDate = date('Y-m-d',strtotime($date));
+			//echo $cMonth = strtoupper(substr($mDate,3,3));
+			//echo $cYear = substr($mDate,0,2);
+			//echo $nMonth = substr($date1,5,2);
+			//echo $expDate = date('Y-m-d',strtotime($date1."+1 month"."-1 day"));
+			
+			//echo 
+			//echo "<br>";
+			//echo $expiryDate = date('Y-m-d', strtotime($date."+15 days"));
+			
+		?>
+		<br>
+	</body>
+</html>
+
+<?php 
+			/*echo $date = date("Y-m-d");
+			echo $NewDate=Date('Y-m-d', strtotime("-7 days"))."<br>";*/
+		?>
+		
+		
+		
+<?php
+
+/*$var = 26.1123;
+
+echo ceil($var);*/
+
+?>
